d98828f1c835f29bc8c368ab3d49704350d99023
Selu04 <150025367+Selu04@users.noreply.github.com>
2025-11-25 02:45:38 +0100

    Formateo

--- lapiconera/src/pages/api/supabaseClient.js
+++ lapiconera/src/pages/api/supabaseClient.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = https://oeakibkrouxtehvwjmlz.supabase.co
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lYWtpYmtyb3V4dGVodndqbWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMDMyNzgsImV4cCI6MjA2MDg3OTI3OH0.79GpR-smy5Jtj25VkJcaQHXziAFMRwZbHjvFls44l-M
+const supabaseUrl = "https://oeakibkrouxtehvwjmlz.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lYWtpYmtyb3V4dGVodndqbWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMDMyNzgsImV4cCI6MjA2MDg3OTI3OH0.79GpR-smy5Jtj25VkJcaQHXziAFMRwZbHjvFls44l-M"
 export const supabase = createClient(supabaseUrl, supabaseKey)
+

004b2f35bfb94832edb0bbdb73dd82e9efc9c241
Selu04 <150025367+Selu04@users.noreply.github.com>
2025-12-07 21:56:01 +0100

    upd: Logo y nombre

--- lapiconera/next.config.mjs
+++ lapiconera/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['oeakibkrouxtehvwjmlz.supabase.co'],
+  },
 };
 
 export default nextConfig;
--- lapiconera/src/pages/_document.js
+++ lapiconera/src/pages/_document.js
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <link rel="icon" href="https://oeakibkrouxtehvwjmlz.supabase.co/storage/v1/object/public/logos/logo_empresa.png" />
+        <title>La Piconera</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-}
+}
